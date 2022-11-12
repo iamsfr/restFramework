e9edd8953bad19b2891960a639837e905b559267
safeer kp <safeerkp262@gmail.com>
2022-11-12 12:21:27 +0530

    second commit urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ItemList,ItemDetail
 
 urlpatterns = [
-    path('item',ItemList.as_view()),
-    path('item/<int:pk>',ItemDetail.as_view()),
+    path('',ItemList.as_view()),
+    path('<int:pk>',ItemDetail.as_view()),
 ]
